9dae2e60311c4d1c7c6ab9a276ab620748bf20cb
Utkarsh <utkarshtiwari102005@gmail.com>
2024-01-19 11:51:07 +0530

    Yes you are done

---
+++ Reverse_array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void reversearray(int arr[], int n){
+    int start = 0;
+    int end = n - 1;
+    while(start < end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    reversearray(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
